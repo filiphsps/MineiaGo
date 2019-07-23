c4e0fd20c3d573c17dae061b17f67d3871a0fa64
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2019-07-23 07:21:55 +0200

    Document function.

--- src/main/java/com/filiphsandstrom/mineiago/utils/ChunkConverter.java
+++ src/main/java/com/filiphsandstrom/mineiago/utils/ChunkConverter.java
@@ -7,6 +7,11 @@ import com.github.steveice10.mc.protocol.data.game.chunk.Chunk;
 import com.github.steveice10.mc.protocol.data.game.chunk.Column;
 import com.github.steveice10.mc.protocol.data.game.world.block.BlockState;
 
+/**
+ * Used to convert Java 1.13+ chunk data to bedrock 1.12+ packet format.
+ * @param column 1.13+ packet chunk data
+ * @return byte[] bedrock 1.12+ chunk.data
+ */
 public class ChunkConverter {
     public byte[] convert(Column column) {
         //TODO: https://github.com/NukkitX/Nukkit/blob/d0568b2535568bd0cc6c8067f4c27b65236133a7/src/main/java/cn/nukkit/level/format/anvil/Anvil.java#L107
