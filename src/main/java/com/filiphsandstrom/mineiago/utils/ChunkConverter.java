544d5af55ba6316c083077f5c68ab3df3104b52f
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2019-07-25 00:55:25 +0200

    Add description to chunkconverter.

--- src/main/java/com/filiphsandstrom/mineiago/utils/ChunkConverter.java
+++ src/main/java/com/filiphsandstrom/mineiago/utils/ChunkConverter.java
@@ -9,41 +9,26 @@ import com.github.steveice10.mc.protocol.data.game.world.block.BlockState;
 
 /**
  * Used to convert Java 1.13+ chunk data to bedrock 1.12+ packet format.
+ * 
  * @param column 1.13+ packet chunk data
  * @return byte[] bedrock 1.12+ chunk.data
  */
 public class ChunkConverter {
     public byte[] convert(Column column) {
-        //TODO: https://github.com/NukkitX/Nukkit/blob/d0568b2535568bd0cc6c8067f4c27b65236133a7/src/main/java/cn/nukkit/level/format/anvil/Anvil.java#L107
+        /*
+         * TODO: implement a section converter that converts the Minecraft Java 1.13+
+         * section format to the Minecraft Bedrock 1.12+ format.
+         * 
+         * The following, while dated can be used for reference. It's still stuck on the
+         * previous Minecraft Java Chunk format that isn't in use anymore but the
+         * bedrock conversion seems to be up-to-date:
+         * https://github.com/NukkitX/Nukkit/blob/d0568b2535568bd0cc6c8067f4c27b65236133a7/src/main/java/cn/nukkit/level/format/anvil/Anvil.java#L107
+         * 
+         * You can also use the following for reference when it comes to the Minecraft Java 1.13+ section format
+         * https://minecraft.gamepedia.com/Talk:Chunk_format
+         */
 
-        Chunk[] sections = column.getChunks();
-        int[] biomeData = column.getBiomeData();
-
-        ByteBuffer stream = ByteBuffer.allocate(2048); // TODO: correct size
-        int count = 0;
-
-        for (int i = sections.length - 1; i >= 0; i--) {
-            if (!sections[i].isEmpty()) {
-                count = i + 1;
-                break;
-            }
-        }
-
-        stream.put((byte) count);
-        for (int i = 0; i < count; i++) {
-            stream.put((byte) 0);
-            
-            List<BlockState> section = sections[i].getBlocks().getStates();
-            for (BlockState b : section) {
-                stream.putInt(b.getId());
-            }
-        }
-
-        for (int i : biomeData) {
-            stream.putInt(i);
-        }
-        stream.put((byte) 0);
-
-        return stream.array();
+        byte[] temp = { 0 };
+        return temp;
     }
 }
